b802518176dca7c4ef2d2a7d5eae1759be987d19
Nikola <nvelinov@my.bcit.ca>
2021-12-10 12:36:08 -0800

    Commit #2 - Set Up Console Log for Submit Button

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,17 @@ let body = document.querySelector(".light-theme")
 toggle.addEventListener('click', (e) => {
     body.classList.toggle('dark-theme')
 })
+
+let submit = document.querySelector("#frm-btn")
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault()
+    let msgvalue = document.querySelector('#msg').value
+    let namevalue = document.querySelector('#name').value 
+    let emailvalue = document.querySelector('#email').value
+    console.log( {
+        name: namevalue,
+        email: emailvalue,
+        msg: msgvalue
+    })
+})
